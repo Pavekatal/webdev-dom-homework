5c94b65c917a140e8c067560bca7aa25ddca5151
Pavekatal <pavekatal@gmail.com>
2025-03-18 19:53:13 +0800

    код получения и отправки комментариев переписан с добавлением цепочки промисов

--- modules/getComments.js
+++ modules/getComments.js
@@ -2,23 +2,22 @@ import { updateComments } from './comments.js'
 import { renderComments } from './renderComments.js'
 import { initAddComments } from './initListeners.js'
 
-export const getComments = async () => {
-    try {
-        const response = await fetch(
-            'https://wedev-api.sky.pro/api/v1/Pavekatal/comments',
-        )
-
-        if (!response.ok) {
-            throw new Error(
-                'Ошибка при получении комментариев: ' + response.statusText,
-            )
-        }
-
-        const data = await response.json()
-        updateComments(data.comments)
-        renderComments()
-        initAddComments(renderComments)
-    } catch (error) {
-        console.error('Произошла ошибка:', error)
-    }
+export const getComments = () => {
+    return fetch('https://wedev-api.sky.pro/api/v1/Pavekatal/comments')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(
+                    'Ошибка при получении комментариев: ' + response.statusText,
+                )
+            }
+            return response.json()
+        })
+        .then((data) => {
+            updateComments(data.comments)
+            renderComments()
+            initAddComments(renderComments)
+        })
+        .catch((error) => {
+            console.error('Произошла ошибка:', error)
+        })
 }
--- modules/postComments.js
+++ modules/postComments.js
@@ -1,17 +1,39 @@
-export const postComments = async (name, text) => {
-    const response = await fetch(
-        'https://wedev-api.sky.pro/api/v1/Pavekatal/comments',
-        {
-            method: 'POST',
-            body: JSON.stringify({ name, text }),
-        },
-    )
+// export const postComments = async (name, text) => {
+//     const response = await fetch(
+//         'https://wedev-api.sky.pro/api/v1/Pavekatal/comments',
+//         {
+//             method: 'POST',
+//             body: JSON.stringify({ name, text }),
+//         },
+//     )
 
-    if (!response.ok) {
-        const errorData = await response.json()
-        throw new Error(errorData.error)
-    }
+//     if (!response.ok) {
+//         const errorData = await response.json()
+//         throw new Error(errorData.error)
+//     }
 
-    const result = await response.json()
-    return result
+//     const result = await response.json()
+//     return result
+// }
+
+export const postComments = (name, text) => {
+    return fetch('https://wedev-api.sky.pro/api/v1/Pavekatal/comments', {
+        method: 'POST',
+        body: JSON.stringify({ name, text }),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                return response.json().then((errorData) => {
+                    throw new Error(errorData.error)
+                })
+            }
+            return response.json()
+        })
+        .then((result) => {
+            return result
+        })
+        .catch((error) => {
+            console.error('Ошибка при отправке комментария', error)
+            throw error
+        })
 }
